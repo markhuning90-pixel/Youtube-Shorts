2630b85b823addb3c37779e217f9fe655b290496
mark huning <302116566+markhuning90-pixel@users.noreply.github.com>
2026-07-10 15:17:05 +0200

    Einfachen Script Generator erstellt

---
+++ generators/script_generator.py
@@ -0,0 +1,2 @@
+def generate_script(topic):
+    return f"In diesem Video erfährst du etwas über: {topic}"
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from generators.topic_picker import get_next_topic
+from generators.script_generator import generate_script
 
 topic = get_next_topic()
 
 if topic is None:
     print("Keine Themen gefunden.")
 else:
-    print("Nächstes Thema:")
+    script = generate_script(topic)
+
+    print("Thema:")
     print(topic)
+
+    print("\nScript:")
+    print(script)
